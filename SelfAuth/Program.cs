ec218653d7fc81dc85416244e3e0160a8ead343a
meni-dev <menisaran15@gmail.com>
2024-10-18 22:47:02 +0530

    Add project files.

---
+++ SelfAuth/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Identity.Client;
+
+Console.WriteLine("Hello, World!");
+
+string clientId = "bbd5737a-1040-477a-81b8-8f7f0711a208";
+
+var app = PublicClientApplicationBuilder.Create(clientId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+var scopes = new string[]
+{
+    "https://graph.microsoft.com/User.Read.All"
+};
+var result = await app.AcquireTokenInteractive(scopes)
+    .ExecuteAsync();
+
+Console.WriteLine(result.AccessToken);
+
+
+Console.ReadLine();
